dc94225873f2db0b93f1b536883b2b90104159a6
Hunter Motte <huntermotte@gmail.com>
2017-10-23 11:49:00 -0400

    date fix

--- usersRouter.js
+++ usersRouter.js
@@ -186,7 +186,7 @@ router.post('/ideas/delete', isAuthenticated, (req, res) => {
 
 // get ideas from other users for explore page-header
 router.get('/ideas/explore', (req, res) => {
-  User.find({}).sort({ideas[0].created: 'desc'})
+  User.find({}).sort('-created')
   .then(users => {
     res.json({users: users.map(
       (user) => user.createExample())
